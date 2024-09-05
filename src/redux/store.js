00b53a0560b3497facf1d824eb6c874672200146
ManhAnhh <ngomanhanh2k3@gmail.com>
2024-09-06 00:45:07 +0700

    use react-toolkit to save data into storage when refreshing do data loss

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,6 +3,8 @@ import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import auth from "./reducers/auth";
 
+// dùng react-persist để lưu dữ liệu vào store sau khi refresh lại trang thì không mất dữ liệu
+// trừ khi dispatch(loggerOut())
 const persistConfig = {
   key: 'alphalink_store',
   storage,
